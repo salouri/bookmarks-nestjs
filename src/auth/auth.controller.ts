832900558b2840c2db0b8625b1e1e4e8a3ace993
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-17 16:11:40 +0300

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth') //global prefix in the url
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup') //  same as "/auth/signup"
+  signup(@Body() dto: AuthDto) {
+    console.log('dto: ', dto);
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin') //  same as "/auth/signin"
+  singin() {
+    return this.authService.signin();
+  }
+}
