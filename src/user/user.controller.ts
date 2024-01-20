3f652b207472bf7238bfd171fff740dac8b19833
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-20 17:42:18 +0300

    Apply the jwt-Strategy on all users' endpoints (using the AuthGuard/JwtGuard)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator/user.decorator';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
+@UseGuards(JwtGuard) // adds the jwt strategy as a middleware ahead of /users/* endpoint
 @Controller('users')
 export class UserController {
   @Get('me')
